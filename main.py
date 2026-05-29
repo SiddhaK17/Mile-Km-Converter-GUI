ba3bfab949881002c54d51fa90949cca290304bc
Siddha Kadam <siddhakad17@gmail.com>
2026-05-30 04:30:42 +0530

    refactor: improve input validation and conversion accuracy
    Added error handling for invalid input in miles to km conversion.

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import os
-os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python313\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'C:\Program Files\Python313\tcl\tk8.6'
-
 from tkinter import *
 
 def miles_to_km():
-    miles = float(miles_input.get())
-    km = round(miles * 1.609)
-    kilometer_result_label.config(text=f"{km}")
+    try:
+        miles = float(miles_input.get())
+        km = round(miles * 1.609, 2)
+        kilometer_result_label.config(text=f"{km}")
+
+    except ValueError:
+        kilometer_result_label.config(text="Invalid")
 
 window = Tk()
 window.title("Miles to Kilometer Converter")
 window.config(padx=20, pady=20)
+window.resizable(False, False)
 
 
 miles_input = Entry(width=7)
 miles_input.grid(column=1, row=0)
+miles_input.insert(0, "0")
 
 miles_label = Label(text="Miles")
 miles_label.grid(column=2, row=0)
@@ -23,7 +25,7 @@ miles_label.grid(column=2, row=0)
 is_equal_label = Label(text="is equal to")
 is_equal_label.grid(column=0, row=1)
 
-kilometer_result_label = Label(text=0)
+kilometer_result_label = Label(text="0")
 kilometer_result_label.grid(column=1, row=1)
 
 kilometer_label = Label(text="Km")
